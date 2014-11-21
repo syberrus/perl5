a237633ddb899dc82a97de2a2aa0c51735ad58a8
Hugo van der Sanden <hv@crypt.org>
2014-11-21 16:25:19 +0000

    study_chunk: extract rck_definep

--- proto.h
+++ proto.h
@@ -7031,6 +7031,12 @@ STATIC void	S_populate_ANYOF_from_invlist(pTHX_ regnode *node, SV** invlist_ptr)
 #define PERL_ARGS_ASSERT_POPULATE_ANYOF_FROM_INVLIST	\
 	assert(node); assert(invlist_ptr)
 
+STATIC bool	S_rck_definep(pTHX_ RExC_state_t *pRExC_state, rck_params_t *params)
+			__attribute__nonnull__(pTHX_1)
+			__attribute__nonnull__(pTHX_2);
+#define PERL_ARGS_ASSERT_RCK_DEFINEP	\
+	assert(pRExC_state); assert(params)
+
 STATIC void	S_rck_elide_nothing(pTHX_ regnode *node)
 			__attribute__nonnull__(pTHX_1);
 #define PERL_ARGS_ASSERT_RCK_ELIDE_NOTHING	\
