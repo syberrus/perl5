589641fc751d8bb6c4adab248d53b7502f75e716
Hugo van der Sanden <hv@crypt.org>
2014-11-28 01:40:34 +0000

    study_chunk: extract rck_eolish

--- proto.h
+++ proto.h
@@ -7081,6 +7081,12 @@ STATIC void	S_rck_enframe(pTHX_ RExC_state_t *pRExC_state, rck_params_t *params,
 #define PERL_ARGS_ASSERT_RCK_ENFRAME	\
 	assert(pRExC_state); assert(params); assert(start); assert(end)
 
+STATIC bool	S_rck_eolish(pTHX_ RExC_state_t *pRExC_state, rck_params_t *params)
+			__attribute__nonnull__(pTHX_1)
+			__attribute__nonnull__(pTHX_2);
+#define PERL_ARGS_ASSERT_RCK_EOLISH	\
+	assert(pRExC_state); assert(params)
+
 STATIC bool	S_rck_exact(pTHX_ RExC_state_t *pRExC_state, rck_params_t *params)
 			__attribute__nonnull__(pTHX_1)
 			__attribute__nonnull__(pTHX_2);
