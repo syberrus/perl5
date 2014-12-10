Leprevost/perl

t/op/print.t: Skip an EBCDIC test
This could be written (the values would probably change depending on the
code page), but the code that would get exercised is unlikely to vary
depending on character set.

t/op/print.t
@@ -12,6 +12,7 @@ fresh_perl_is('$_ = qq{OK\n}; print STDOUT;', "OK\n",
               'print with only a filehandle outputs $_');
 SKIP: {
     skip_if_miniperl('no dynamic loading of PerlIO::scalar in miniperl');
+    skip("EBCDIC") if $::EBCDIC;    # Varies depending on code page
 fresh_perl_is(<<'EOF', "\xC1\xAF\xC1\xAF\xC1\xB0\xC1\xB3", "", "print doesn't launder utf8 overlongs");
 use strict;
 use warnings;

syberrus/perl5

t/op/print.t
@@ -13,6 +13,7 @@ fresh_perl_is('$_ = qq{OK\n}; print STDOUT;', "OK\n", {},
               'print with only a filehandle outputs $_');
 SKIP: {
     skip_if_miniperl('no dynamic loading of PerlIO::scalar in miniperl');
+    skip("EBCDIC") if $::IS_EBCDIC;    # Varies depending on code page
 fresh_perl_is(<<'EOF', "\xC1\xAF\xC1\xAF\xC1\xB0\xC1\xB3", {}, "print doesn't launder utf8 overlongs");
 use strict;
 use warnings;